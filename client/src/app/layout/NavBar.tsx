0761864c3326a6e371cfc886c0b2c3eaf37ff449
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-07-29 23:56:42 +1000

    Frontend for Catalog Completed

---
+++ client/src/app/layout/NavBar.tsx
@@ -0,0 +1,23 @@
+
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { DarkMode, LightMode } from "@mui/icons-material";
+
+type Props = {
+  toggleDarkMode: () => void;
+  darkMode: boolean;
+}
+
+export default function NavBar({darkMode, toggleDarkMode}:Props) {
+
+  return (
+    <AppBar position="fixed">
+        <Toolbar>
+            <Typography variant="h6">Ski Store AU</Typography>
+            <IconButton onClick={toggleDarkMode}>
+              {darkMode ? <DarkMode /> : <LightMode sx={{color:'yellow'}} /> }
+            </IconButton>
+        </Toolbar>
+
+    </AppBar>
+  )
+}
