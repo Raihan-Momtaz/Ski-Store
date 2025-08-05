281226369d1907aaf3072c8638de456e592109ce
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-08-06 02:19:06 +1000

    Use of Redux for globsl state management

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { decrement, increment} from "./counterReducer"
+import { Button, ButtonGroup, Typography } from "@mui/material"
+import {useAppDispatch, useAppSelector } from "../../app/store/store";
 
 export default function ContactPage() {
+  const {data} = useAppSelector(state => state.counter)
+  const dispatch = useAppDispatch();
   return (
-    <div>ContactPage</div>
+    <>
+      <Typography variant="h2">
+        Contact page
+      </Typography>
+      <Typography variant="body1">
+        The data is: {data}
+      </Typography>
+      <ButtonGroup>
+        <Button onClick={() => dispatch(decrement(1))} color = "error">D</Button>
+        <Button onClick={() => dispatch(increment(1))} color = "secondary">I</Button>
+        <Button onClick={() => dispatch(increment(5))} color = "secondary">I</Button>
+      </ButtonGroup>
+
+    </>
   )
 }
