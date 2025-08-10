cd6e498eb12df80215afe7e5e428e65bf2299c6d
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-08-10 12:09:26 +1000

    Order Summary done and checkout page created

---
+++ client/src/features/checkout/CheckoutPage.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+
+export default function CheckoutPage() {
+  return (
+    <Typography>
+        Only authorizrd users should be able to see this.
+    </Typography>
+    
+  )
+}
