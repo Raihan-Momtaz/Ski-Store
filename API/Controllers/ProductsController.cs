a3439bcf9f626fcda126045b06d1f9047599ac0a
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-08-10 15:20:11 +1000

    Pagination page number and size added

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,18 +11,22 @@ namespace API.Controllers
     public class ProductsController(StoreContext context) : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string? orderBy,
-                                                                string? searchTerm,
-                                                                string? brands,
-                                                                string? types)
+        public async Task<ActionResult<List<Product>>> GetProducts(
+                        [FromQuery]ProductParams productParams)
+        
         {
             var query = context.Products
-                        .Sort(orderBy)
-                        .Search(searchTerm)
-                        .Filter(brands, types)
+                        .Sort(productParams.OrderBy)
+                        .Search(productParams.SearchTerm)
+                        .Filter(productParams.Brands, productParams.Types)
                         .AsQueryable();
 
-            return await query.ToListAsync();
+            var products = await PagedList<Product>.ToPagedList(query,
+                productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.Metadata);
+
+            return products;
         }
 
         [HttpGet("{id}")] // api/products/2
---
+++ API/RequestHelpers/PaginationParams.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.Net.Http.Headers;
+
+namespace API.RequestHelpers;
+
+public class PaginationParams
+{
+    private const int MaxPageSize = 50;
+
+    public int PageNumber { get; set; } = 1;
+
+    public int _pageSize = 8;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+    }
+}
