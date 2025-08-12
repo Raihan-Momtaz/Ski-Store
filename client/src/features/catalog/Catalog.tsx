9532099a1346abb5ff227ce38434e7020672259e
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-08-12 22:41:54 +1000

    Filters froentend skeleton created

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,6 +1,8 @@
 
+import { Grid2 } from "@mui/material";
 import ProductList from "./ProductList";
 import { useFetchProductsQuery } from "./catalogApi";
+import Filters from "./FIlters";
 
 
 
@@ -10,8 +12,13 @@ export default function Catalog() {
  const {data, isLoading} = useFetchProductsQuery();
  if(isLoading || !data) return <div>Loading...</div>
   return (
-    <>
-        <ProductList products={data}/>
-      </>
+    <Grid2 container spacing={4}>
+        <Grid2 size={3}>
+            <Filters/>
+        </Grid2 >
+        <Grid2 size={9}>
+          <ProductList products={data}/>
+        </Grid2>
+    </Grid2>
   )
 }
