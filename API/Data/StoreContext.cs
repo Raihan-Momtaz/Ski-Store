805845ac5d452429c97e6a5fc55303f68eba5211
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-08-14 11:28:10 +1000

    Setup identity and user address functionality

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,17 +1,27 @@
 using System;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Build.Framework;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class StoreContext(DbContextOptions options) : DbContext(options)
+public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
     public required DbSet<Product> Products { get; set; }
 
     public required DbSet<Basket> Baskets { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        base.OnModelCreating(modelBuilder);
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityRole>()
+            .HasData(
+                new IdentityRole{Id="db5a161d-43ab-49c9-8156-c863ef8a28bb",Name = "Member", NormalizedName = "MEMBER"},
+                new IdentityRole{Id="0462ba47-f16a-4a5a-83ed-bf974d2520ed",Name = "Admin", NormalizedName = "ADMIN"}
+            );
+
     }
 }
