96e4354671a52bee515e9d01a909be89d5328b40
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-09-01 01:33:52 +1000

    create update delete api created

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 
 
 builder.Services.AddCors();
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddTransient<ExceptionMiddleware>();
 builder.Services.AddScoped<PaymentsService>();
 builder.Services.AddIdentityApiEndpoints<User>(opt =>
